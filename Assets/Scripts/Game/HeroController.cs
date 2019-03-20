c1ba0308af639e7882dee928811d00f4fdf2388c
Mirous12 <mirousmisaaki@gmail.com>
2019-03-20 23:07:21 +0200

    Fixed animation bugs. Added Orc prefab

--- Assets/Scripts/Game/HeroController.cs
+++ Assets/Scripts/Game/HeroController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeroController : MonoBehaviour 
+public class HeroController : MonoBehaviour
 {
     private enum LookDirector
     {
@@ -10,62 +10,87 @@ public class HeroController : MonoBehaviour
         Right
     }
 
+    [Range(3.0f, 10.0f)]
     public float Speed = 2.0f;
+    [Range(6.0f, 16.0f)]
+    public float JumpStrength = 7.0f;
 
-    private bool Jumping { get; set; }
+    private bool _jumping;
+    private bool Jumping
+    {
+        get => _jumping;
+
+        set
+        {
+            currentAnimator.SetBool( "IsJumping", value );
+            _jumping = value;
+        }
+    }
 
     private Animator currentAnimator;
     private LookDirector currentLook;
 
-	void Start () 
+    void Start()
     {
         currentAnimator = GetComponent<Animator>();
         currentLook = LookDirector.Left;
 
         Jumping = false;
-	}
-	
-	void FixedUpdate () 
-    {
-        float currentAxes = Input.GetAxis("Horizontal");
+    }
 
-        //TODO: Animator Set
+    void FixedUpdate()
+    {
+        float currentAxes = Input.GetAxis( "Horizontal" );
+
+        if( currentAxes == 0f )
+        {
+            currentAnimator.SetBool( "IsWalking", false );
+        }
+        else
+        {
+            currentAnimator.SetBool( "IsWalking", true );
+        }
+
+        if( Jumping )
+        {
+            currentAnimator.SetBool( "IsWalking", false );
+        }
 
         Vector3 positionToSet = new Vector3();
 
-        positionToSet.x = (Speed * currentAxes) * Time.fixedDeltaTime;
+        positionToSet.x = ( Speed * currentAxes ) * Time.fixedDeltaTime;
 
         transform.position = transform.position + positionToSet;
 
         LookDirector newLook = LookDirector.Left;
 
-        if (positionToSet.x > 0)
+        if( positionToSet.x > 0 )
         {
             newLook = LookDirector.Right;
         }
 
-        if (currentLook != newLook)
+        if( currentLook != newLook && !currentAnimator.GetBool( "Attack" ) )
         {
-            Vector3 scaleFactor = new Vector3(-transform.localScale.x, 1, 1);
+            Vector3 scaleFactor = new Vector3( -transform.localScale.x, 1, 1 );
             transform.localScale = scaleFactor;
 
             currentLook = newLook;
         }
 
         //Jump
-        if (Input.GetKey(KeyCode.Space) && !Jumping)
+        if( Input.GetKey( KeyCode.Space ) && !Jumping )
         {
             Jumping = true;
 
             var rigidbody = GetComponent<Rigidbody2D>();
 
-            rigidbody.AddRelativeForce(new Vector2(1, 7), ForceMode2D.Impulse);
+            rigidbody.AddRelativeForce( new Vector2( 1, JumpStrength ), ForceMode2D.Impulse );
         }
-	}
+    }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D( Collision2D collision )
     {
-        if (collision.gameObject.name == "Ground")
+        if( collision.gameObject.name == "Ground" )
         {
             Jumping = false;
         }
---
+++ Assets/Scripts/Game/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public int Damage = 1;
+
+    private void OnTriggerEnter2D( Collider2D collision )
+    {
+        var enemy = collision.gameObject.GetComponent<Enemy>();
+
+        if( enemy != null )
+        {
+            
+        }
+    }
+}
