d2e2771d96b68559df03ef1f8be0561514525cbd
Mirous12 <mirousmisaaki@gmail.com>
2019-03-21 20:26:27 +0200

    Added EnemyManager And GameManagerInstantiator

--- Assets/Scripts/Game/Enemy.cs
+++ Assets/Scripts/Game/Enemy.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int MaxHealth;
+
+    public delegate void OnSingleEnemyDied(GameObject enemy);
+    public event OnSingleEnemyDied SingleEnemyDied;
+
+    private int _currentHealth;
+    private int CurrentHealth
+    {
+        get { return _currentHealth; }
+
+        set
+        {
+            _currentHealth = value;
+
+            if (_currentHealth <= 0)
+            {
+                _currentHealth = 0;
+                SingleEnemyDied(gameObject);
+            }
+        }
+    }
+
     void Start()
+    {
+        var camera = GameObject.Find("Main Camera");
+
+        if (camera != null)
+        {
+            GameManagerInstantiator gmInit = camera.GetComponent<GameManagerInstantiator>();
+
+            if (gmInit != null)
+            {
+                EnemyManager enemyManager = gmInit.GetEnemyManager();
+
+                if (enemyManager != null)
+                {
+                    enemyManager.SubscribeToNewEnemy(this);
+                }
+            }
+        }
+
+        CurrentHealth = MaxHealth;
+    }
+
+    
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SendDamage(int damage)
     {
-        
+        if (CurrentHealth > 0)
+        {
+            CurrentHealth -= damage;
+        }
     }
 }
