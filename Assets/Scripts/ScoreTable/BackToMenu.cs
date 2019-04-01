488504c0d0f173e2bac423822bc28f260d361c75
Mirous12 <mirousmisaaki@gmail.com>
2019-04-01 19:30:46 +0300

    Added new scene - score table

---
+++ Assets/Scripts/ScoreTable/BackToMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick( PointerEventData eventData )
+    {
+        if( eventData.pointerCurrentRaycast.gameObject.name == gameObject.name )
+        {
+            SceneManager.LoadScene( "SampleScene" );
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreTable/TryAgain.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class TryAgain : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick( PointerEventData eventData )
+    {
+        if( eventData.pointerCurrentRaycast.gameObject.name == gameObject.name )
+        {
+            SceneManager.LoadScene( "GameScene" );
+        }
+    }
+}
